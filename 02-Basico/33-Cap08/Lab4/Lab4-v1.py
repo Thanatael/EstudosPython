6573b49f21cbecfd68be2c3ec5c087a5ec57da89
Thanatael <william777200800@gmail.com>
2024-11-03 16:34:47 -0300

    commit outubro

--- 02-Basico/33-Cap08/Lab4/Lab4-v1.py
+++ 02-Basico/33-Cap08/Lab4/Lab4-v1.py
@@ -3,6 +3,17 @@
 
 # Import
 import random
+from os import system, name
+
+# Função para limpar a tela a cada execução
+def limpa_tela():
+    
+     # Windows
+     if name == 'nt':
+          _ = system('cls')
+     # Linux e MacOS
+     else:
+          _ = system('clear')
 
 # Board (tabuleiro)
 board = ['''
@@ -70,8 +81,23 @@ O   |
 class Hangman:
 
 	# Método Construtor
+     def __init__(self, palavra):
+          self.palavra = palavra
+          self.letras_erradas = []
+          self.letras_escohlidas = []
 
 	# Método para adivinhar a letra
+     def guess(self, letra):
+
+          if letra in self.palavra and letra not in self.letras_escohlidas:
+               self.letras_escohlidas.append(letra)
+
+          elif letra not in self.palavra and letra not in self.letras_erradas:
+               self.letras_erradas.append(letra)
+          else:
+               return False
+          
+          return True
 	
 	# Método para verificar se o jogo terminou
 		
@@ -80,4 +106,4 @@ class Hangman:
 	# Método para não mostrar a letra no board
 		
 	# Método para checar o status do game e imprimir o board na tela
-    print("oi")
+     print("oi")
